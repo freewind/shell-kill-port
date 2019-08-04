7f8eee75076b6871d513705197fb2ee1e558ba4a
Peng Li <peng.li@compass.com>
2019-08-04 14:18:26 +0800

    add webpack

--- hello.ts
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env npx ts-node
-
-const message: string = 'Hello';
-console.log(message);
---
+++ webpack.config.ts
@@ -0,0 +1,23 @@
+import {Configuration} from 'webpack';
+import path from 'path';
+
+const config: Configuration = {
+  mode: "development",
+  entry: './entry.ts',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js'
+  },
+  resolve: {
+    extensions: ['.ts', '.js']
+  },
+  module: {
+    rules: [{
+      test: /\.ts$/,
+      loader: 'ts-loader',
+      exclude: /node_modules/
+    }]
+  },
+}
+
+export default config;
